97509ec9b6ff9343048bb7cd4e978e12afb8c8ad
eric wang <eric.wang@wine-searcher.local>
2019-11-11 15:57:41 +1300

    naming convensions

--- src/com/winesearcher/org/numberguessing/guessers/DigitGuesser.java
+++ src/com/winesearcher/org/numberguessing/guessers/DigitGuesser.java
@@ -2,35 +2,33 @@ package com.winesearcher.org.numberguessing.guessers;
 
 public class DigitGuesser implements Guesser {
 	private int[] nextGuess;
-	private int[][] matrix;
-	private int xPointer = 3;
-	private int yPointer = 0;
-	private int currentRightDigitCount = 0;
+	private int currentPosition = 3;
+	private int digitOnCurrentPosition = 0;
+	private int previousCorrectDigit = 0;
 
 	public DigitGuesser() {
-		initMatrix();
 		nextGuess = new int[] { 0, 0, 0, 0 };
 	}
 
 	@Override
-	public int guessFromClues(int rightDigitCount) {
-		if (rightDigitCount == -1) {
+	public int guessFromClues(int correctDigit) {
+		if (correctDigit == -1) {
 			return 0;
-		} else if (currentRightDigitCount > rightDigitCount) {
-			nextGuess[xPointer] = yPointer - 1;
-			xPointer = xPointer - 1;
-			yPointer = 1;
-			nextGuess[xPointer] = yPointer;
-		} else if (currentRightDigitCount < rightDigitCount) {
-			nextGuess[xPointer] = yPointer;
-			xPointer = xPointer - 1;
-			yPointer = 1;
-			nextGuess[xPointer] = yPointer;
+		} else if (previousCorrectDigit > correctDigit) {
+			nextGuess[currentPosition] = digitOnCurrentPosition - 1;
+			currentPosition = currentPosition - 1;
+			digitOnCurrentPosition = 1;
+			nextGuess[currentPosition] = digitOnCurrentPosition;
+		} else if (previousCorrectDigit < correctDigit) {
+			nextGuess[currentPosition] = digitOnCurrentPosition;
+			currentPosition = currentPosition - 1;
+			digitOnCurrentPosition = 1;
+			nextGuess[currentPosition] = digitOnCurrentPosition;
 		} else {
-			yPointer = yPointer + 1;
-			nextGuess[xPointer] = yPointer;
+			digitOnCurrentPosition = digitOnCurrentPosition + 1;
+			nextGuess[currentPosition] = digitOnCurrentPosition;
 		}
-		currentRightDigitCount = rightDigitCount;
+		previousCorrectDigit = correctDigit;
 		int result = getIntFromDigitArray(nextGuess);
 		System.out.println("Guesser guessed " + result);
 		return result;
@@ -42,14 +40,5 @@ public class DigitGuesser implements Guesser {
 			result += Math.pow(10, (intArray.length - i - 1)) * intArray[i];
 		}
 		return result;
-	}
-
-	private void initMatrix() {
-		matrix = new int[4][10];
-		for (int i = 0; i < 4; i++) {
-			for (int j = 0; j < 10; j++) {
-				matrix[i][j] = j;
-			}
-		}
 	}
 }
